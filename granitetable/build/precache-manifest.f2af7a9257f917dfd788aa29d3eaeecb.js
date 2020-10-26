99c03274a37bc79d59dd1b9370e46928e7275cd6
Rams3ska <responsetie@mail.ru>
2020-10-26 20:21:46 +0700

    builded

---
+++ granitetable/build/precache-manifest.f2af7a9257f917dfd788aa29d3eaeecb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ea2f5ca13dd8d6ade02b9908e8b62d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "03523c26ddf3538d65c9",
+    "url": "./static/css/main.aa41584b.chunk.css"
+  },
+  {
+    "revision": "4942255d70c22cc17369",
+    "url": "./static/js/2.7531e465.chunk.js"
+  },
+  {
+    "revision": "fba25877d77d2443a8e0b025f2b01c34",
+    "url": "./static/js/2.7531e465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03523c26ddf3538d65c9",
+    "url": "./static/js/main.cfeb5839.chunk.js"
+  },
+  {
+    "revision": "b21e1f25d673ce9de1c7",
+    "url": "./static/js/runtime-main.3fd9f32d.js"
+  },
+  {
+    "revision": "3b28b0d97422eced54d255069045f1c9",
+    "url": "./static/media/1231414.3b28b0d9.png"
+  },
+  {
+    "revision": "56307b1aa5586e57c749a7e24fa125e4",
+    "url": "./static/media/2.56307b1a.jpg"
+  },
+  {
+    "revision": "111a44389b6d8aeef212166207a54774",
+    "url": "./static/media/68c16041fdcf2f72dbe48506f694f15b.111a4438.jpg"
+  },
+  {
+    "revision": "13909fb592f4ef63b507bf45c2fde7c1",
+    "url": "./static/media/cambria-brittanicca.13909fb5.jpg"
+  },
+  {
+    "revision": "0d1c6e183e5fb66830eeef10b0079e2a",
+    "url": "./static/media/list.0d1c6e18.svg"
+  },
+  {
+    "revision": "202733b2a8de5c85c196e6456a1732a5",
+    "url": "./static/media/stoleshnicy-7hpkzk.202733b2.jpg"
+  },
+  {
+    "revision": "14849a2ed05ad6f471d01489782e6caa",
+    "url": "./static/media/stone10.14849a2e.jpg"
+  },
+  {
+    "revision": "2069fd6c324ac7736540578a14c35b44",
+    "url": "./static/media/unnamed (1).2069fd6c.jpg"
+  },
+  {
+    "revision": "2d85b8d7c9c8b911946fd4f61be9ec6f",
+    "url": "./static/media/unnamed.2d85b8d7.jpg"
+  }
+]);
